f6867a2deeea6044749efc9542b6d077f289db6b
shawky <mastershawky2017@gmail.com>
2026-01-19 22:59:54 +0200

    update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
-const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
