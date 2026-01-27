86297e84bd006c55bf0cff69f1f9196f6a776758
shawky <mastershawky2017@gmail.com>
2026-01-27 19:07:55 +0200

    add login controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -46,8 +46,31 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePicture: user.profilePicture,
+        });
+        
+    } catch (error) {
+        console.log("Error in login", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const logout = (req, res) => {
