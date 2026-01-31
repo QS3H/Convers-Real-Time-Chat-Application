d80638038eba64afa32bd3cf38e3694c4d6d5026
shawky <mastershawky2017@gmail.com>
2026-01-31 23:28:21 +0200

    add update profile router

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -81,3 +81,11 @@ export const logout = (req, res) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const updateProfile = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
