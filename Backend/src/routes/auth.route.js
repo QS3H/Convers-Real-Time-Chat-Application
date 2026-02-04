ff9403640235b06c48a0f646c7d0f8559e3ad58c
shawky <mastershawky2017@gmail.com>
2026-02-04 23:10:54 +0200

    update auth route

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -9,7 +9,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
-
 router.get("/check-auth", protectRoute, checkAuth);
 
 export default router;
