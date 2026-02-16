7552f29f5c212ad82c5b642174a082dcc9290486
shawky <mastershawky2017@gmail.com>
2026-02-16 23:05:27 +0200

    update app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,14 @@ import Navbar from './components/Navbar';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import SignUpPage from './pages/SignUpPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 import { useAuthStore } from './store/useAuthStore';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const { authUser, checkAuth, isAuthenticated } = useAuthStore();
@@ -34,11 +35,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
