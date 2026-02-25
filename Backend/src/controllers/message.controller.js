6d3338393f94f74cacc6dec04467d59484034890
shawky <mastershawky2017@gmail.com>
2026-02-25 04:03:55 +0200

    Added realtime functionality

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudinary from "cloudinary";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -52,6 +53,13 @@ export const sendMessage = async(req, res) => {
      });
 
      await newMessage.save();
+
+    //  Realtime functionality
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+        // io.to(<socket_id>).emit() used to send events to specific client
+        io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
      res.status(201).json(newMessage);
     } catch (error) {
